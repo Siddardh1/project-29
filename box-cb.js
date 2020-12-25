27f75abadb598b2f6955bd3730557eb49451f732
Siddardh1 <74356653+Siddardh1@users.noreply.github.com>
2020-12-25 13:20:25 +0530

    Add files via upload

---
+++ box-cb.js
@@ -0,0 +1,18 @@
+class Box3  {
+  constructor(x, y){
+    var options = {
+      'restitution':0.8,
+      'friction':1.0,
+      'density':0.1
+  }
+  this.body = Bodies.rectangle(x,y,40,50,options);
+  World.add(world, this.body);
+  }
+  display(){
+    fill("cyan")
+    rectMode(CENTER);
+    rect(this.body.position.x,this.body.position.y,40,50);
+   
+    
+  }
+};
---
+++ box-pb.js
@@ -0,0 +1,18 @@
+class Box2  {
+  constructor(x, y){
+    var options = {
+      'restitution':0.8,
+      'friction':1.0,
+      'density':0.1
+  }
+  this.body = Bodies.rectangle(x,y,40,50,options);
+  World.add(world, this.body);
+  }
+  display(){
+    fill("lightpink")
+    rectMode(CENTER);
+    rect(this.body.position.x,this.body.position.y,40,50);
+   
+    
+  }
+};
